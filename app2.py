f2691f3d0e41f6d39c6cecd9ea7d7f40ac1066e7
denhermi <denhermi24@gmail.com>
2024-03-29 15:49:57 +0100

    initial commit

---
+++ app2.py
@@ -0,0 +1,13 @@
+import streamlit as st 
+
+def main():
+    st.title('Application Hermione WIADA 2023')
+    user = st.text_input('Entrer votre nom')
+    if st.button('Dis bonjour'):
+        if user :
+            st.write(f'Bonjour {user} ')
+        else :
+            st.write('Bonjour à tous')
+
+if __name__ == '__main__':
+    main()
